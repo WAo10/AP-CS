9e589226fde03ee3fec6cad7c9a9316a4beb9896
William T Ao <WTAo@student.naperville203.org>
2017-08-30 09:03:57 -0500

    Here's an outline
    hahaha

---
+++ outline.java
@@ -0,0 +1,49 @@
+//William Ao
+//Date:
+//Name:
+//Program:
+
+/* Variable List
+
+*/
+
+import java.util.Scanner;
+import java.text.NumberFormat;
+import java.text.DecimalFormat;
+import java.text.*;//*is a wildcard, importing anything you use in program
+
+public class 			//must have a class that is the same name as the file name
+{
+
+
+public static void main (String[] args)	// must have main
+{
+
+
+
+
+
+}//end of main
+
+
+
+}//end of class firstprog
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
