275a6aaa540a19c567955f341213180f5175ec83
William T Ao <WTAo@student.naperville203.org>
2017-09-20 08:46:51 -0500

    mario no

---
+++ outline.java
@@ -0,0 +1,48 @@
+//William Ao
+//Date:
+//Program:
+
+/* Variable List
+
+*/
+
+import java.util.Scanner;
+import java.text.NumberFormat;
+import java.text.DecimalFormat;
+import java.text.*;//*is a wildcard, importing anything you use in program
+
+public class 			//must have a class that is the same name as the file name
+{
+
+
+public static void main (String[] args)	// must have main
+{
+
+
+
+
+
+}//end of main
+
+
+
+}//end of class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
